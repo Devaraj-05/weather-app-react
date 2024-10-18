545a42caba127df02eea31194aee13e1e71a427c
Devaraj <padma.devaraj1800@gmail.com>
2024-10-18 19:44:30 +0530

    API Key is hidden

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=51a6082cdcbd9a9bb5618d8cfcc446d3
-`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
